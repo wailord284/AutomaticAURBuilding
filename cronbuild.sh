64f747b7027aa544e36b96331c02a819a8e64fd7
Alex Gaudino <wailord284@gmail.com>
2022-04-03 15:57:21 -0700

    Move ccache here

--- cronbuild.sh
+++ cronbuild.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#By setting this, some packages that are built will search for ccache
+#This is not required, but may speed up building for some applications
+#This can also be enforced for ALL packages by enabling ccache in makepkg.conf
+export USE_CCACHE=1
 #Automatically run the aurbuild script
 cd /home/alex/Scripts
 #Build aur packages
@@ -7,4 +11,5 @@ wait
 sleep 10s
 #Upload aur packages
 /home/alex/Scripts/aurupload-V2.sh
+echo "Done uploading!"
 sleep 30s
