150154e73d97a7ac708246ae401e2c67bdcf7132
Alex Gaudino <wailord284@gmail.com>
2022-04-10 16:06:10 -0700

    Improve aurremove

--- aurremove.sh
+++ aurremove.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
+#Function to check array contents
+#https://stackoverflow.com/questions/3685970/check-if-a-bash-array-contains-a-value
+arrayContains () {
+	local e match="$1"
+	shift
+	for e; do [[ "$e" == "$match" ]] && return 0; done
+	return 1
+}
+
 #Package files
 aurGitPackages="/home/alex/Scripts/aurgitpackages.txt"
 aurPackages="/home/alex/Scripts/aurpackages.txt"
-repoPackageDirectory="/mnt/aurbuild/AurmageddonRepo"
+repoPackageDirectory="/mnt/aurbuild/AurmageddonRepo/packages"
 repoBuildDirectory="/mnt/aurbuild/AurmageddonBuild"
+#Create array of both package files
+mapfile -t allPackages < <(cat $aurPackages $aurGitPackages | sort -u)
+
 
 #Take user input for package to remove
-read -p "Package to remove: " rmPackage
+read -r -p "Package to remove: " rmPackage
 
-#Try to remove the package from both package lists
-sed -i /"$rmPackage"/d "$aurPackages"
-sed -i /"$rmPackage"/d "$aurGitPackages"
+#Check if the package exists before doing anything
+arrayContains "$rmPackage" "${allPackages[@]}"
+if [ $? = 0 ]; then
+	#Remove the package from both package lists if rmPackage is in allPackages
+	sed -i /"$rmPackage"/d "$aurPackages"
+	sed -i /"$rmPackage"/d "$aurGitPackages"
 
-#Remove the package file from the repo
-rm -v "$repoPackageDirectory"/packages/"$rmPackage"*
-rm -r "$repoBuildDirectory"/"$rmPackage"*
-
-echo -e "\nRemoved: $rmPackage"
+	echo -e "\nThe following items have been removed: "
+	#Remove the package and build files from the repo
+	rm -v "$repoPackageDirectory/$rmPackage"*
+	rm -r "$repoBuildDirectory/$rmPackage"*
+	echo "$repoBuildDirectory/$rmPackage"
+else
+	echo "$rmPackage cannot be found in $aurPackages or $aurGitPackages"
+fi
