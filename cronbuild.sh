f4da3af480a1bd1cb0ae8590b7fdb06bf06bb550
Alex Gaudino <wailord284@gmail.com>
2022-05-12 14:27:53 -0700

    Comment CCACHE

--- cronbuild.sh
+++ cronbuild.sh
@@ -2,7 +2,7 @@
 #By setting this, some packages that are built will search for ccache
 #This is not required, but may speed up building for some applications
 #This can also be enforced for ALL packages by enabling ccache in makepkg.conf
-export USE_CCACHE=1
+#export USE_CCACHE=1
 #Automatically run the aurbuild script
 cd /home/alex/Scripts
 #Build aur packages
